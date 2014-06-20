f4cc2a3ee66a19ec3002650efa05422150d8e0b7
elfnor <elfnor@gmail.com>
2014-06-20 15:46:18 +1200

    ignore txt files

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://elfnor.github.io/lookthinkmake/'
+SITEURL = 'http://elfnor.github.io/lookthinkmake'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
